61da4a23c32792acbb1daa419fa30570292550da
Rchang <39424482+changchangv@users.noreply.github.com>
2019-11-20 21:10:31 -0500

    Update and rename NthHighestSalary.sql to 176&177. NthHighestSalary.sql

--- 176&177. NthHighestSalary.sql
+++ 176&177. NthHighestSalary.sql
@@ -1,19 +1,35 @@
-# select the second highest salary
-#select max(salary) from salaries
-#
 #For example, given the above Employee table, the nth highest salary where n = 2 is 200. 
 #If there is no nth highest salary, then the query should return null
- 
+
+#Leetcode 176 easy
+# select the second highest salary
+
+select max(salary) from salaries
+where salary < (select max(salary) from salaries);
+
+
+# Leetcode 177 medium
+# select the Nth highest salary 
+
 delimiter $$
+
 create function NthHighestSalary(n int) returns int
+
 begin
+
 declare result int;
 declare m int;
 set m=n-1;
+
 	select distinct salary into result from salaries
-    order by salary desc
-    limit m,1;
+	order by salary desc
+	limit m,1;
+	
 return result;
 end$$
+
 delimiter ;
+
+# n = 3: select the third highest salary
+# n = 2: select the second highest salary
 select NthHighestSalary(3);  
